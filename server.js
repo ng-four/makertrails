d967d4e360ee91c92b19f34d97fc4a717e12b67c
Daniel <dela1000@gmail.com>
2016-01-23 14:17:03 -0800

    [feature] add server, add gitignore file

---
+++ server.js
@@ -0,0 +1,38 @@
+var mysql = require('mysql');
+var express = require('express');
+var session = require('express-session');
+
+//Middleware
+var parser = require('body-parser');
+// var router = require('./routes.js');
+
+// Router
+var app = express();
+module.exports.app = app;
+
+// Establish session
+// app.use(session({
+//  secret: 'dgdjkgd34',
+//  resave: true,
+//  saveUninitialized: false,
+//  cookie: {maxAge: 1000*60*60}
+// }));
+
+// Set what we are listening on.
+app.set("port", process.env.PORT || 8000);
+
+// Logging and parsing
+app.use(parser.json());
+
+// Serving static files from client directory.
+
+app.use(express.static(__dirname + '/client/'));
+
+// Set up our routes
+// app.use("/", router);
+
+if (!module.parent) {
+    app.listen(app.get("port"));
+    console.log("Listening on", app.get("port"));
+}
+
