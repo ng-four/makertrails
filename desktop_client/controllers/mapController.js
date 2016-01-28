7567262c728a8d605b08a72ddaa93a7f59efa383
Daniel <dela1000@gmail.com>
2016-01-27 19:19:57 -0800

    [feature] add index.html for the desktop client.

--- desktop_client/controllers/mapController.js
+++ desktop_client/controllers/mapController.js
@@ -51,11 +51,12 @@ $(document).ready(function(){
   map = new GMaps({
     div: '#map',
     lat: 34.0192316,
-    lng: -118.4943091
+    lng: -118.4943091,
+    zoom: 15
   });
 
   GMaps.on('marker_added', map, function(marker) {
-    $('#markers-with-index').append('<li><a href="#" class="pan-to-marker" data-marker-index="' + map.markers.indexOf(marker) + '">' + marker.title + '</a></li>');
+    // $('#markers-with-index').append('<li><a href="#" class="pan-to-marker" data-marker-index="' + map.markers.indexOf(marker) + '">' + marker.title + '</a></li>');
 
     $('#markers-with-coordinates').append('<li><a href="#" class="pan-to-marker" data-marker-lat="' + marker.getPosition().lat() + '" data-marker-lng="' + marker.getPosition().lng() + '">' + marker.title + '</a></li>');
   });
@@ -73,7 +74,7 @@ $(document).ready(function(){
       lng: lng
     })
 
-    console.log("+++ 76  Desktop_CLient mapController.js: ", JSON.stringify(locationsSelected, null, "\t"));
+    console.log("+++ 76 Desktop_CLient mapController.js: ", JSON.stringify(locationsSelected, null, "\t"));
 
     var template = $('#edit_marker_template').text();
 
@@ -82,7 +83,7 @@ $(document).ready(function(){
     map.addMarker({
       lat: lat,
       lng: lng,
-      title: 'Marker #' + index,
+      title: 'Selected Location ' + index, //Here's where the marker gets its name. We should make this editable so users can name the location whatever they want (to fill POST Body: "name")
       infoWindow: {
         content : content
       }
