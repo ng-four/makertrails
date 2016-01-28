f356c33b7bb70278d7e13e7687d9deaef7ead1f0
luisgamero <gamero.luis.enrique@gmail.com>
2016-01-28 11:42:45 -0800

    [feature] Add collision detection factory

--- makertrails_client/www/js/services.js
+++ makertrails_client/www/js/services.js
@@ -1,10 +1,31 @@
 angular.module('app.services', [])
 
-.factory('BlankFactory', [function(){
+.factory('CollisionFactory', function() {
+  var toRadians = function(deg) {
+    return deg * Math.PI / 180;
+  };
 
-}])
+  var haversine = function(lat1, lng1, lat2, lng2) {
+    lat1 = toRadians(lat1);
+    lng1 = toRadians(lng1);
+    lat2 = toRadians(lat2);
+    lng2 = toRadians(lng2);
 
-.service('BlankService', [function(){
+    var dlat = lat1 - lat2;
+    var dlng = lng1 - lng2;
 
-}]);
+    var angle = math.pow(math.sin(dlat / 2), 2) + math.cos(lat1) * math.cos(lat2) * math.pow(math.sin(dlng / 2), 2);
+    var circle = 2 * math.atan2(math.sqrt(angle), math.sqrt(1 - angle));
+    var distance = 6367000 * circle;
 
+    return distance;
+  };
+
+
+  return {
+    withinRange: function(lat1, lng1, lat2, lng2, m) {
+      var dist = haversine(lat1, lng1, lat2, lng2);
+      return dist ? dist <= m : false;
+    }
+  };
+});
