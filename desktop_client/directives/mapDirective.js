a2e1e75c7a0c96109b1e145a71f7a668d35dbf7f
Niicck <nickivons@yahoo.com>
2016-01-30 09:05:59 -0800

    [fix][createMap] name new locations in order

--- desktop_client/directives/mapDirective.js
+++ desktop_client/directives/mapDirective.js
@@ -4,43 +4,33 @@ angular.module("App") //placeholder name
 function trailMap(MapFactory) {
   var link = function($scope, $el){
     //el is an array of the element (plus its children) where this directive is used
-    console.log("What is $el", $el)
     var $map = $scope.map
-    // var map = new GMaps({
-    //   div: '#map',
-    //   lat: 34.0192316,
-    //   lng: -118.4943091,
-    //   zoom: 15
-    // });
 
     GMaps.on('click', $map.map, function(event) {
       var index = $map.markers.length;
       var lat = event.latLng.lat();
       var lng = event.latLng.lng();
 
-      // lat/lng added on click, sent to array
-      $scope.selectedLocations.push({
+      var newLocation = {
         lat: lat,
         lng: lng,
         name: "Location " + (index + 1),
         editing: false
-      })
+      }
+      // lat/lng added on click, sent to array
+      $scope.selectedLocations.push(newLocation)
       $scope.$apply();
       console.log("+++ 76 Desktop_CLient mapController.js: ", JSON.stringify($scope.selectedLocations, null, "\t"));
 
-      _.each($scope.selectedLocations, function(location){
-        $map.addMarker({
-          lat: location.lat,
-          lng: location.lng,
-          title: location.name, //Here's where the marker gets its name. We should make this editable so users can name the location whatever they want (to fill POST Body: "name")
-          infoWindow: {
-            content : location.name
-          }
-        });
+      $map.addMarker({
+        lat: newLocation.lat,
+        lng: newLocation.lng,
+        title: newLocation.name, //Here's where the marker gets its name. We should make this editable so users can name the location whatever they want (to fill POST Body: "name")
+        infoWindow: {
+          content : newLocation.name
+        }
       });
-
     });
-
 
   };
   return {
