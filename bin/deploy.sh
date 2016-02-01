414666544bc5bdea8d2eecc1c47008b73d3871c9
Niicck <nickivons@yahoo.com>
2016-02-01 11:18:12 -0800

    [fix] no longer delete and recreate deployment branch. Must manually merge

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,10 +2,10 @@
 
 set -o errexit # Exit on error
 
-git branch -d deploy
-git checkout -b deploy
+#git branch -D deploy
+#git checkout -b deploy
 echo 'mobile_client/ ' >> .gitignore
 git add .gitignore
 git commit -m '[deploy] add mobile client to .gitignore'
 git push heroku deploy:master #&& sed -i '' -e '$d' .gitignore
-git checkout dev
+#git checkout dev
