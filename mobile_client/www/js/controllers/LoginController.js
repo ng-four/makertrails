cc11b5fb0c7961c7821fe70f56d1b8a37c3a169c
Juan Schaar <schaar.juan@gmail.com>
2016-02-01 20:43:44 -0800

    [Progress]Added Auth routes, controllers, and factories

--- mobile_client/www/js/controllers/LoginController.js
+++ mobile_client/www/js/controllers/LoginController.js
@@ -1,21 +1,72 @@
 angular.module('app.LoginController', [])
 
-.controller('LoginController', function($scope, $location, LoginFactory, $state, $ionicPopup) {
-      $scope.login = function(){
-      LoginFactory.login($scope.username, $scope.password)
-    }
-    // $scope.data = {};
- 
-    // $scope.login = function() {
-    //     LoginFactory.loginUser($scope.data.username, $scope.data.password).success(function(data) {
-    //       $state.go('/makerMap');
-    //       }).error(function(data) {
-    //         var alertPopup = $ionicPopup.alert({
-    //           title: 'Login Failed',
-    //           template: 'Please check your credentials'
-    //         });
-    //       });
-    //     console.log("LOGIN user: " + $scope.data.username + " - PW: " + $scope.data.password);
-    //     $location.url('/makerMap');
-    //   }
+.controller('AuthController', function($scope, $location, $state, $ionicPopup, LoginFactory, AUTH_EVENTS) {
+    $scope.username = AuthService.username();
+
+    $scope.$on(AUTH_EVENTS.notAuthorized, function(event){
+        var alertPopup = $ionicPopup.alert({
+            title: 'Unauthorized!',
+            template: 'You are not allowed to access this page'
+        });
+    });
+
+    $scope.$on(AUTH_EVENTS.notAuthenticated, function(event){
+        AuthService.logout();
+        $state.go('login');
+        var alertPopup = $ionicPopup.alert({
+            title: 'Session Lost',
+            template: 'Sorry, you have to login again'
+        });
+    });
+
+    $scope.setCurrentUsername = function(name){
+        $scope.username = name;
+    };
+})
+.controller('LoginController', function($scope, $state, $ionicPopup, AuthService){
+    $scope.data = {};
+
+    $scope.login = function(data){
+      AuthService.login(data.username, data.password)
+      .then(function(authenticated){
+        $scope.go('mapMaker', {}, {reload: true});
+        $scope.setCurrentUsername(data.username);
+      },
+      function(err){
+        var alertPopup = $ionicPopup.alert({
+        title: "Login Failed",
+        template: 'Please check your credentials'
+      });
     })
+  };
+})
+.controller('AuthRoutes', function($scope, $state, $http, $ionicPopup, AuthService){
+
+  $scope.logout = function(){
+    AuthService.logout();
+    $state.go('login');
+  };
+
+  $scope.performValidRequest = function(){
+    $http.get('http://makertrails.herokuapp.come/valid')
+    .then(function(result){
+      $scope.response = result;
+    });
+  }
+
+  $scope.performUnauthorizedRequest = function(){
+    $http.get('http://makertrails.herokuapp.come/notauthorized')
+    .then(function(result){},
+      function(err){
+        $scope.response = err;
+    });
+  };
+
+  $scope.performInvalidRequest = function(){
+    $http.get('http://makertrails.herokuapp.come/notauthenticated')
+    .then(function(result){},
+    function(err){
+      $scope.response = err;
+    });
+  };
+});
