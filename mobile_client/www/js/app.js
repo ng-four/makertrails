81bb6c2092aa43d1a00598ca8e1c4b8846c87cac
Juan Schaar <schaar.juan@gmail.com>
2016-02-01 20:43:44 -0800

    [Progress]Added Auth routes, controllers, and factories

--- mobile_client/www/js/app.js
+++ mobile_client/www/js/app.js
@@ -25,4 +25,24 @@ angular.module('app', [
       StatusBar.styleDefault();
     }
   });
-});
+})
+.run(function($rootScope, $state, AuthService, AUTH_EVENTS){
+
+  $rootScope.$on('$stateChangesStart' , function(event, next, nextParams, fromState) {
+
+    if('data' in next && 'authorizedRoles' in next.data){
+      var authorizedRoles = next.data.authorizedRoles;
+      if(!AuthService.isAuthorized(authorizedRoles)){
+        event.preventDefault();
+        $state.go($state.current, {}, {reload: true});
+        $rootScope.$broadcast(AUTH_EVENTS.notAuthorized);
+      }
+    }
+    if(!AuthService.isAuthenticated()){
+      if(next.name !== 'login'){
+        event.preventDefault();
+        $state.go('login');
+      }
+    }
+  });
+})
--- mobile_client/www/js/factories/LoginFactory.js
+++ mobile_client/www/js/factories/LoginFactory.js
@@ -1,59 +1,84 @@
 angular.module('app.LoginFactory', [])
-.factory('LoginFactory', function($http, $state){
-    var login = function(username, password){
-      return $http ({
-        method: 'POST',
-        url: 'http://makertrails.herokuapp.com/login',
-        data: {
-          username: username,
-          password: password
-        }
-      })
-      .then(function(success){
-        $state.go('/makerMap') // THIS WILL REDIRECT TO HOME
-      }, function(err){
-        console.log(err);
-      })
-    };
-    var signup = function(username, password, email){
-      return $http ({
-        method: 'POST',
-        url: 'http://makertrails.herokuapp.com/signup',
-        data: {
-          username: username,
-          password: password,
-          email: email
-        }
-      })
-      .then(function(success){
-        $state.go('makerMap') // THIS WILL REDIRECT TO HOME
-      }, function(err){
-        console.log(err);
-      })
-    };
-    return{
-      login: login,
-      signup: signup
-    }
-  // return {
-  //   loginUser: function(name, password){
-  //     var deferred = $q.defer();
-  //     var promise = deferred.promise;
+.factory('LoginFactory', function($q, $http, USER_ROLES){
+  var LOCAL_TOKEN_KEY = 'yourTokenKey';
+  var username = '';
+  var isAuthenticated = false;
+  var role = '';
+  var authToken;
 
-  //     if(name === 'user' && password === 'password'){
-  //       deferred.resolve('Welcome '+ name);
-  //     }else{
-  //       deferred.reject('incorrect user name and password');
-  //     }
-  //     promise.success = function(callback){
-  //       promise.then(callback);
-  //       return promise;
-  //     }
-  //     promise.error = function(callback){
-  //       promise.then(null, callback);
-  //       return promise;
-  //     }
-  //     return promise;
-  //   }
-  // }
+  function loadUserCredentials(){
+    var token = window.localStorage.getItem(LOCAL_TOKEN_KEY);
+    if(token){
+      userCredentials(token);
+    }
+  }
+
+  function storeUserCredentials(token) {
+    window.localStorage.setItem(LOCAL_TOKEN_KEY, token){
+      userCredentials(token);
+    }
+  }
+
+  function userCredentials(token){
+    username = token.split('.')[0];
+    isAuthenticated = true;
+    authToken = token;
+
+    if(username == 'admin'){
+      role = USER_ROLES.admin
+    }
+    if(username == 'user'){
+      role = USER_ROLES.public
+    }
+
+    $http.defaults.headers.common['X-Auth-Token'] = token;
+  }
+
+  var login = function(name, password){
+    return $q(function(resolve, reject){
+      if((name == 'admin' && password === '1') || (name == 'user' && password == '1')) {
+        storeUserCredentials(name + '.yourServerToken');
+        resolve('Login Sucess');
+      }else{
+        reject('Login Failed');
+      }
+    });
+  };
+
+  var logout = function(){
+    destroyUserCredentials();
+  };
+
+  var isAuthorized = function(authorizedRoles){
+    if(!angular.isArray(authorizedRoles)){
+      authorizedRoles = [authorizedRoles];
+    }
+    return (isAuthenticated && authorizedRoles.indexOf(role) !== -1);
+  };
+
+  loadUserCredentials();
+
+  return {
+    login: login,
+    logout: logout,
+    isAuthorized: isAuthorized,
+    isAuthenticated: function(){return isAuthenticated;},
+    username: function(){return username;},
+    role: function(){return role;}
+  };
+})
+
+.factory('AuthInterceptor', function($rootScope, $q, AUTH_EVENTS){
+  return {
+    responseError: function(response){
+      $rootScope.$broadcast({
+        401: AUTH_EVENTS.notAuthenticated,
+        403: AUTH_EVENTS.notAuthorized
+      }[response.status], response);
+      return $q.reject(response);
+    }
+  };
+})
+.config(function($httpProvider){
+  $httpProvider.interceptors.push('AuthInterceptor');
 });
