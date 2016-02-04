621b1bfe9442ef9dcb750fc032f9c377a77734ed
luisgamero <gamero.luis.enrique@gmail.com>
2016-02-04 15:41:01 -0800

    [refactor] Progress on mobile auth refactoring

--- mobile_client/www/js/factories/SelectMapFactory.js
+++ mobile_client/www/js/factories/SelectMapFactory.js
@@ -1,20 +1,24 @@
 angular.module('app.SelectMapFactory', [])
 
-.factory('SelectMapFactory', function($http){
+.factory('SelectMapFactory', function($http) {
   var allMaps;
 
   function displayMaps() {
     return $http({
-      method: 'GET',
-      url: 'http://makertrails.herokuapp.com/mapInfo'
-    })
-    .then(function(data){
-      allMaps = data.data.allMaps;
-      return allMaps;
-    });
+        method: 'GET',
+        url: 'http://makertrails.herokuapp.com/mapInfo',
+        "headers": {
+          "content-type": "application/json",
+          "makertrails-token": window.localStorage['makertrails-token'];
+        }
+      })
+      .then(function(data) {
+        allMaps = data.data.allMaps;
+        return allMaps;
+      });
   };
 
-  function selectMap(mapID){
+  function selectMap(mapID) {
     return mapID;
   }
   return {
