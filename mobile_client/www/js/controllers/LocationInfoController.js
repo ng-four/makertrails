c3e8b5e80d7b1f36e601245fa6be53f6b973afaf
Juan Schaar <schaar.juan@gmail.com>
2016-02-06 13:54:34 -0800

    [fix]merge conflict

---
+++ mobile_client/www/js/controllers/LocationInfoController.js
@@ -0,0 +1,15 @@
+angular.module('app.LocationInfoController', [])
+.controller('LocationInfoController', function($scope, $state, LocationInfoFactory){
+  $scope.data = {};
+  $scope.locationInfo = function(){
+    
+  };
+
+  $scope.locationPhotos = function(){
+
+  };
+
+  $scope.locationReviews = function(){
+
+  };
+});
