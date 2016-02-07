709ce1bef62abe1b64f5f02d8275005304a94754
Daniel <dela1000@gmail.com>
2016-02-06 16:02:23 -0800

    [test] deploy to test heroku

--- server/helpers/utils.js
+++ server/helpers/utils.js
@@ -3,8 +3,10 @@ var session = require('express-session');
 var jwt  = require('jwt-simple');
 
 exports.formatProgress = function(locations, progresses) {
+  console.log("+++ 6 utils.js locations: ", locations)
   for (var i=0; i < locations.length; i++) {
     var progress = _.findWhere(progresses, {location_id: locations[i].id});
+    console.log("+++ 9 utils.js progress: ", progress)
     locations[i].dataValues.visited = progress.visited;
     locations[i].dataValues.progress_id = progress.id;
   }
