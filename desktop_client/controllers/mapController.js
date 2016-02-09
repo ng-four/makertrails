59a37f2034e71c4c0858f0165e7465799157de9d
Niicck <nickivons@yahoo.com>
2016-02-08 16:41:54 -0800

    [feature] set max on create map locations

--- desktop_client/controllers/mapController.js
+++ desktop_client/controllers/mapController.js
@@ -13,15 +13,27 @@ function MapController($scope, MapFactory){
     lng: -118.4943091,
     zoom: 15
   });
+  navigator.geolocation.getCurrentPosition(function(position) {
+    $scope.map.setCenter(position.coords.latitude, position.coords.longitude);
+    $scope.map.addMarker({
+      lat: position.coords.latitude,
+      lng: position.coords.longitude,
+      icon: 
+      title: "You are here!",
+      infoWindow: {
+        content : "You are here!"
+      }
+    });
+  });
   $scope.createMap = function(){
     if($scope.selectedLocations.length !== 0){
       MapFactory.createMap($scope.mapInfo, $scope.selectedLocations)
       .then(function (success) {
         $scope.selectedLocations = [];
         MapFactory.refreshMap($scope.selectedLocations, null, $scope.map)
-        $scope.mapInfo = {
-          "user": 1 //Hardcoded until backend is fixed
-        };
+        // $scope.mapInfo = {
+        //   "user": 1 //Hardcoded until backend is fixed
+        // };
       })
     }
   }
