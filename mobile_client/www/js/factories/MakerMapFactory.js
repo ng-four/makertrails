7df1eb66e4f7a83ed97ade49d0629578ae812003
Niicck <nickivons@yahoo.com>
2016-02-08 20:05:06 -0800

    [fix] merge conflicts in MakerMap Factory

--- mobile_client/www/js/factories/MakerMapFactory.js
+++ mobile_client/www/js/factories/MakerMapFactory.js
@@ -28,7 +28,6 @@ function makerMapFactory($http, $q, $state, $ionicLoading, $ionicPopup, $statePa
     };
 
     //creates default map with above options
-    // scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
     return new google.maps.Map(document.getElementById("map"), mapOptions);
   };
 
@@ -69,88 +68,8 @@ function makerMapFactory($http, $q, $state, $ionicLoading, $ionicPopup, $statePa
     return q.promise;
   }
 
-  var theRestOfIt = function(scope, locations, map){
-        var myLocation = null;
-
-        //sets interval to track changes in user position
-        var watchPositionTracker = navigator.geolocation.watchPosition(userLocationChange, userLocationError);
-
-        //success callback for navigator.geolocation.watchPosition()
-        function userLocationChange(pos) {
-          var lat = pos.coords.latitude;
-          var lon = pos.coords.longitude;
-          var currentLatLng = new google.maps.LatLng(lat, lon);
-          var collided = false;
-
-          for (var i = 0; i < locations.length; i++) {
-            var currentLocation = locations[i];
-            if (CollisionFactory.withinRange(lat, lon, locations[i].lat, locations[i].lon, 100)) {
-              if (!currentLocation.visited){
-                //Alert for first time ever
-                // var alertPopup = $ionicPopup.alert({
-                //   template: 'Collision!! at ' + locations[i].progress_id
-                // });
-
-                $http.put(url + '/progress', {
-                  'progressId': currentLocation.progress_id
-                }, {
-                  'Content-Type': 'application/json'
-                })
-                .then(function(data) {
-                  console.log("COLLISION!", data)
-                  currentLocation.visited = true;
-                  deleteMarkers(markers);
-                  setMarkers(scope.locations, map, markers);
-                }, function(err) {
-                  console.log(err);
-                })
-              }
-              collided = true;
-              scope.collision.contact = true;
-              scope.collision.locationID = currentLocation.id;
-              var alertPopup = $ionicPopup.alert({
-                template: 'Collision!! at ' + currentLocation.progress_id
-              });
-              alertPopup.then(function(res) {
-               console.log('Tapped!', res);
-               window.navigator.geolocation.clearWatch(watchPositionTracker); //stop watchPosition
-               $state.go('locationInfo', {
-                 currentMap: $stateParams.mapID.id,
-                 currentLocation: scope.collision.locationID
-               }, {reload: true});
-               return;
-            });
-            }
-          }
-
-          if (collided===false){
-            scope.collision = null;
-          }
-
-          map.setCenter(currentLatLng);
-          if (myLocation !== null) {
-            myLocation.setPosition(currentLatLng);
-          } else {
-            myLocation = new google.maps.Marker({
-              position: currentLatLng,
-              animation: google.maps.Animation.DROP,
-              icon: {
-                path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
-                fillColor: "blue",
-                fillOpacity: 1,
-                strokeWeight: 0,
-                scale: 5
-              },
-              map: map
-            });
-          }
-          $ionicLoading.hide();
-        }
-
-        //error callback for navigator.geolocation.watchPosition()
-        function userLocationError(err) {
-          console.log("user location failed", err);
-        }
+  var userLocationError = function(err) {
+    console.log("user location failed", err);
   }
 
   return {
@@ -159,6 +78,5 @@ function makerMapFactory($http, $q, $state, $ionicLoading, $ionicPopup, $statePa
     setMarkers: setMarkers,
     getMapLocations: getMapLocations,
     userLocationError: userLocationError
-    // theRestOfIt: theRestOfIt
   };
 }
